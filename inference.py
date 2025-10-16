b113f6fb45ece1973206353bccb89f9bcba1877b
Lasse Lauwerys <lauweryslasse@gmail.com>
2025-10-17 00:08:36 +0200

    aa

--- inference.py
+++ inference.py
@@ -1,10 +1,34 @@
 from ultralytics import YOLO
+import cv2
 
 # https://docs.ultralytics.com/tasks/classify/#predict
 
-if __name__ == '__main__':
-  # Load a model
-  model = YOLO("runs/detect/train11/weights/best.pt") # Replace with trained model path
+BATCH_SIZE = 5
+IMAGE_SIZE = 320
 
-  # Predict with the model
-  results = model(source='videos/14_55/14_55_front_cropped.mp4', conf=0.25, save=True, imgsz=320, batch=100, device=0)  # predict on an image
+SKIP_FRAMES = 5
+
+if __name__ == '__main__':
+    model = YOLO("runs/detect/train11/weights/best.pt")
+    
+    cameras = [
+        {'name': 'front', 'video_path': 'videos/14_55/14_55_front_cropped.mp4'},
+        {'name': 'back_left', 'video_path': 'videos/14_55/14_55_back_left_cropped.mp4'},
+        {'name': 'back_right', 'video_path': 'videos/14_55/14_55_back_right_cropped.mp4'},
+        {'name': 'back_right', 'video_path': 'videos/14_55/14_55_top_cropped.mp4'}
+    ]
+    
+    video_path = 'videos/14_55/14_55_front_cropped.mp4'
+    
+    results = model.track(source=video_path, conf=0.25, save=True, imgsz=IMAGE_SIZE, batch=BATCH_SIZE, device=0)
+    
+    for result in results:
+        print(result.boxes)
+        if result.boxes is not None and result.boxes.id is not None:
+            boxes = result.boxes.xywh.cpu()
+            track_ids = result.boxes.id.cpu().numpy().astype(int)
+            confidences = result.boxes.conf.cpu()
+            
+            print("Boxes:", boxes)
+            print("Track IDs:", track_ids)
+            print("Confidences:", confidences)
--- train.py
+++ train.py
@@ -3,11 +3,7 @@ from ultralytics import YOLO
 # https://docs.ultralytics.com/tasks/classify/#train
 
 if __name__ == '__main__':
-    # Load a model
-    # model = YOLO("yolo11n-cls.yaml")  # build a new model from YAML
-    model = YOLO("yolo11n.pt")  # load a pretrained model (https://github.com/ultralytics/assets/releases/download/v8.3.0/yolo11n-cls.pt)
-    # model = YOLO("yolo11n-cls.yaml").load("yolo11n-cls.pt")  # build from YAML and transfer weights
-
-    # Train the model
+    model = YOLO("yolo11n.pt")
+    
     results = model.train(data="dataset.yaml", epochs=10, imgsz=320, save=True, workers=2, batch=16, device=0)  # train the model
     print(results)

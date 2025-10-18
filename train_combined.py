31d9a6bb26a75e912314b7d41a3608f970140440
Lasse Lauwerys <lauweryslasse@gmail.com>
2025-10-18 06:21:35 +0200

    amp?

--- train_combined.py
+++ train_combined.py
@@ -5,5 +5,5 @@ from ultralytics import YOLO
 if __name__ == '__main__':
     model = YOLO("yolo11m.pt")
     
-    results = model.train(data="combined_dataset.yaml", epochs=200, imgsz=320, save=True, workers=2, batch=40, device=0)  # train the model
+    results = model.train(data="combined_dataset.yaml", epochs=200, imgsz=320, save=True, workers=2, batch=40, lr0=0.01, amp=True, device=0)  # train the model
     print(results)
